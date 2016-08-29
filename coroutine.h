16599f1f09648ebf9b1df93b65342c296716a145
xinxin <282209415@qq.com>
2016-08-29 17:12:43 +0800

    Update coroutine.h

--- coroutine.h
+++ coroutine.h
@@ -14,9 +14,9 @@ struct schedule * coroutine_open(void);   // 创建协程调度器
 void coroutine_close(struct schedule *);  // 关闭协程调度器
 
 int coroutine_new(struct schedule *, coroutine_func, void *ud);   // 创建协程任务,将其加入调度器中
-void coroutine_resume(struct schedule *, int id);                 // 重入协程号为id的协程任务
+void coroutine_resume(struct schedule *, int id);                 // 恢复协程号为id的协程任务
 int coroutine_status(struct schedule *, int id);                  // 根据协程任务id返回协程的当前状态
 int coroutine_running(struct schedule *);                         // 返回调度器S中正在running的协程任务id
-void coroutine_yield(struct schedule *);                          // 保持上下文后中断当前协程的执行
+void coroutine_yield(struct schedule *);                          // 保存当前上下文后中断当前协程的执行
 
 #endif
